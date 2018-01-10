c856972b0a94f5d35d91c1f288dd5562efb0f83c
Lindsey Johnson <redhook@hotmail.com>
2018-01-09 17:08:22 -0700

    changed song selector and styling

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ class Jukebox {
 	constructor(){
 		this.songlist = [];
 		this.currentSong;
+		this.songNames = ['Cookies', 'That Song Again', 'Suspense', 'some song'];
 	}
 	addSong(song){
 		this.songlist.push(song);
+		console.log(this.songlist);
+		
 		
 	}
 	loadSong(songs){
@@ -22,8 +25,8 @@ class Jukebox {
 	songPause(){
 		this.currentSong.pause();
 	}
-	displaySong(){
-
+	displaySong(id){
+		$('#songName').html(this.songNames[id]);
 	}
 	
 	findSong(title){
@@ -34,15 +37,25 @@ class Jukebox {
 			 }
 		}
 	}
+	listAllSongs(){
+			for (var i = 0; i < this.songNames.length; i++) {
+			$('.list_of_songs').append(`<li><a href="#"class="songs" data-count="${i}"> ${this.songNames[i]} </a></li>`);
+			}
+	}
+
+	getSong(id){
+		this.findSong(this.songlist[id]);
+	}
+
 }//ends jukebox
 	
 
 let music = new Jukebox();
-music.addSong('rock_on.mp3');
-music.addSong('say_something.mp3');
-music.addSong('how_great_thou_art.mp3');
+music.addSong('Cookies.mp3');
+music.addSong('that_song.mp3');
+music.addSong('suspense.mp3');
 music.loadSong(music.songlist[0]);
-
+music.listAllSongs();
 document.getElementById('playButton').onclick = function(){ 
 	music.songPlay();
 };
@@ -52,6 +65,17 @@ document.getElementById('pauseButton').onclick = function(){
 document.getElementById('stopButton').onclick = function(){ 
 	music.songStop();
 };
-document.getElementById('submitButton').onclick = function(){ 
-	music.findSong(document.getElementById('songName').value);
-};
+// document.getElementById('submitButton').onclick = function(){ 
+// 	music.findSong(document.getElementById('songName').value);
+// };
+
+$(document).on('click', '.songs', function() {
+		let id = $(this).data('count');
+		music.songStop();
+		music.getSong(id);
+		music.displaySong(id);
+		music.songPlay();
+	});
+
+	// music.listAllSongs();
+
